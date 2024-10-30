1218406523d3779f2dc0c6ba91282dfd72483809
jeru <john.ungab7@gmail.com>
2024-10-30 23:53:51 +0800

    edit: file structure

---
+++ Assets/Scripts/utils/LoginValidator.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+
+public class LoginValidator : MonoBehaviour
+{
+    public TMP_InputField username;
+    public TMP_InputField password;
+    public GameObject emptyFieldError, passwordError, emailError; 
+}
