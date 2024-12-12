cf75e10917d9eb521f6b5f628deb4a0a7a40fceb
jeru <john.ungab7@gmail.com>
2024-12-12 22:51:16 +0800

    edit: managers

--- Assets/Scripts/managers/AudioManager.cs
+++ Assets/Scripts/managers/AudioManager.cs
@@ -2,18 +2,55 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance; // singleton approach
     public AudioSource musicSource;
     public AudioSource sfxSource;
 
     public AudioClip clickSoundClip;
     public AudioClip backgroundMusicClip;
+    public AudioController audioController;
+    private Settings settings;
+
+    void Awake()
+    {
+        // the 'gameObject' automatically initialized when you attach this AudioManager
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        InitializeVolume();
+    }
 
     /// <summary>
-    /// Initializes the audio settings from the player's stored settings.
+    /// initializes volume on start
     /// </summary>
-    /// <param name="musicVolume"></param>
+    public void InitializeVolume()
+    {
+        settings = Settings.Instance;
+
+        if (settings == null)
+        {
+            Debug.Log("Settings is null");
+        }
+
+        Debug.Log("Settings SFX: " + settings.GetSfx() + "Settings Music: " + settings.GetMusic());
+
+        musicSource.volume = settings.GetMusic();
+        sfxSource.volume = settings.GetSfx();
+    }
+
+    /// <summary>
+    /// syncs player's volume settings into system's volume settings
+    /// </summary>
     /// <param name="sfxVolume"></param>
-    public void InitializeVolume(float musicVolume, float sfxVolume)
+    /// <param name="musicVolume"></param>
+    public void SyncVolume(float sfxVolume, float musicVolume)
     {
         musicSource.volume = musicVolume;
         sfxSource.volume = sfxVolume;
@@ -24,9 +61,12 @@ public class AudioManager : MonoBehaviour
     /// </summary>
     public void PlayMusic()
     {
-        musicSource.clip = backgroundMusicClip;
-        musicSource.loop = true;
-        musicSource.Play();
+        if (!musicSource.isPlaying)
+        {
+            musicSource.clip = backgroundMusicClip;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
     }
 
     /// <summary>
